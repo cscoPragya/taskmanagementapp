04b2c4b24235730edf0a7fdb7b2ddb4f1d27a9a0
cscoPragya <pragyarajput459@gmail.com>
2025-11-02 13:16:13 +0530

    add env

--- backend/server.js
+++ backend/server.js
@@ -5,19 +5,23 @@ const cors = require('cors');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ["https://managetasks-app.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.log('MongoDB connection error:', err));
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log('MongoDB connection error:', err));
 
 // Use auth routes
 const authRoutes = require('./routes/auth');
-const taskRoutes= require('./routes/taskRoutes')
+const taskRoutes = require('./routes/taskRoutes')
 app.use('/api', authRoutes);
-app.use('/api',taskRoutes);
+app.use('/api', taskRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
